ed6808c341664188b5499f35d6cace525f21eebc
Anton Antonov <anton.antonov@castle.co>
2017-06-09 18:17:27 +0400

    dotenv

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,3 @@
-if ENV['TEST_COV']
-  require 'simplecov'
-  SimpleCov.start 'rails' do
-    command_name 'Manual Test'
-    coverage_dir 'app/views/coverage'
-  end
-end
-
 require_relative 'boot'
 
 require 'rails/all'
@@ -13,6 +5,16 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+Dotenv::Railtie.load
+
+if ENV['TEST_COV']
+  require 'simplecov'
+  SimpleCov.start 'rails' do
+    command_name "Manual Tests PID #{$$}"
+    coverage_dir 'app/views/coverage'
+  end
+end
 
 module SimpleCovPoc
   class Application < Rails::Application
